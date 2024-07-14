7d837d5ca5187804e13b3e1df81b764cb879ba59
Dulina jayarathna <jayarathnadulina@gmail.com>
2024-07-14 16:59:52 +0530

    implement travel variables with travel tips

--- info/urls.py
+++ info/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from info import views
 
 urlpatterns = [
-    path("travel-tip/",views.travel_tip)
+    path("travel-tip/", views.travel_tip),
+    path("travel-tip/<str:destination>/", views.travel_tip_for_destination)
 ]
--- info/views.py
+++ info/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def travel_tip(request):
     response = {"tip": "always take your passport"}
     return JsonResponse(response)
+
+
+def travel_tip_for_destination(request, destination):
+    response = {"tip": f"Here are the details about {destination}"}
+    return JsonResponse(response)
