042685d823780fe30d1910daba8fa51212c355b9
Dulina jayarathna <jayarathnadulina@gmail.com>
2024-07-14 17:18:41 +0530

    implement travel variables with travel tips

--- info/views.py
+++ info/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def travel_tip(request):
-    response = {"tip": "always take your passport"}
+    dest = request.GET.get("des", None)
+    if dest:
+        response = {"tip": f"Here are the details about {dest}"}
+    else:
+        response = {"tip": "always take your passport"}
     return JsonResponse(response)
 
 
